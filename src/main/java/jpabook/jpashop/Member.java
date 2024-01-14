f6e16cb0fb999602f15c641ab95d33da13461068
성동식 <tjdehdtlr123@naver.com>
2024-01-14 16:07:06 +0900

    인텔j랑 Git 연동

---
+++ src/main/java/jpabook/jpashop/Member.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //
+    private Long id;
+    private String username;
+}
